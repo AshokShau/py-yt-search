284ea847641a6e20726cb5141cd78515da5a8502
AshokShau <114943948+AshokShau@users.noreply.github.com>
2025-02-22 16:43:13 +0530

    fix import err and bump version

--- py_yt/__init__.py
+++ py_yt/__init__.py
@@ -5,8 +5,10 @@ from .search import (
     Search, VideosSearch, ChannelsSearch, PlaylistsSearch, CustomSearch, ChannelSearch
 )
 
+from .handlers import ComponentHandler, RequestHandler
+
 __all__ = [
     "Video", "Playlist", "Suggestions", "Hashtag", "Comments", "Transcript", "Channel",
     "Search", "VideosSearch", "ChannelsSearch", "PlaylistsSearch", "CustomSearch",
-    "ChannelSearch"
+    "ChannelSearch", "ComponentHandler", "RequestHandler"
 ]
